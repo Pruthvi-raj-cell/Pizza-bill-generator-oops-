766c1809e351314b85bf9e1a17b04c64006f62cd
Pruthvi-raj-cell <mpruthvimain1104@gmail.com>
2026-05-26 16:28:47 +0530

    few changes

--- src/Pizza.java
+++ src/Pizza.java
@@ -2,6 +2,9 @@ public class Pizza {
 
    private String inputPizzaType;
    private int basPrice;
+   final private int extraCheesePrice = 100;
+   final private int extraToppingsPrice = 100;
+   final private int takeAwayPrice = 20;
 
    public Pizza(String inputPizzaType){
 
@@ -16,5 +19,25 @@ public class Pizza {
    public void getPizzaPrice(){
        System.out.println(this.basPrice);
    }
+
+   public void extraCheese(){
+       System.out.println("extra cheese has been added");
+       this.basPrice += extraCheesePrice;
+   }
+
+   public void extraTopping(){
+       System.out.println("extra toppings has been added");
+       this.basPrice += extraToppingsPrice;
+
+   }
+
+   public void takeAway(){
+    this.basPrice += takeAwayPrice;
+   }
+
+   public void getBill(){
+       System.out.println(this.basPrice);
+   }
+
 
 }
